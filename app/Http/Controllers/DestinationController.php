f7f77cc3668b2cae9787af38a58c6c36572ebbd5
Fabian Ramirez <fabianrm@gmail.com>
2024-09-29 10:50:30 -0500

    Modificaciones en material por destino

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -73,9 +73,8 @@ class DestinationController extends Controller
 
     public function getMaterialsByDestination($destinationId)
     {
-        $materials = OutputDetail::select('outputs.date', 'materials.code', 'materials.name', 'brands.name as brand','materials.model', 'presentations.name as presentation', 'output_details.quantity', 'output_details.subtotal')
-        ->join('entry_details', 'output_details.entry_detail_id', '=', 'entry_details.id')
-        ->join('materials', 'entry_details.material_id', '=', 'materials.id')
+        $materials = OutputDetail::select('outputs.date', 'materials.code', 'materials.name', 'brands.name as brand', 'presentations.name as presentation', 'output_details.quantity', 'output_details.subtotal')
+        ->join('materials', 'output_details.material_id', '=', 'materials.id')
         ->join('presentations', 'presentations.id', '=', 'materials.presentation_id')
         ->join('brands', 'brands.id', '=', 'materials.brand_id')
         ->join('outputs', 'output_details.output_id', '=', 'outputs.id')
